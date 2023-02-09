9a3e35b3a4485b2462ed9f4cbd489d3884d24af3
Houroux <arthurtrovato@gmail.com>
2023-02-09 15:21:03 +0100

    feat: ajout html et css de la modale et du bouton modifier

--- FrontEnd/assets/logged.js
+++ FrontEnd/assets/logged.js
@@ -3,14 +3,19 @@ const header = document.querySelector('header');
 const modeEditeurHeader = document.querySelector('.mode-editeur-header');
 
 
+// A APPLIQUER EN MODE EDITION
+// header.style.margin = '80px 0 50px 0';
+// modeEditeurHeader.style.display = 'flex';
+// 
+// header.style.margin = '50px 0 50px 0';
+// modeEditeurHeader.style.display = 'none';
+
+
+
 if (localStorage.getItem('token')) {
     baliseLogin.innerHTML = 'logout';
-    header.style.margin = '80px 0 50px 0';
-    modeEditeurHeader.style.display = 'flex';
 } else {
     baliseLogin.innerHTML = 'login';
-    header.style.margin = '50px 0 50px 0';
-    modeEditeurHeader.style.display = 'none';
 }
 
 
